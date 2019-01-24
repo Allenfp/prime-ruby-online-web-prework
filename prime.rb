cbf2e4d42f7e7c3c85eb1f1dee7ec131c760babc
Allen F Partlow <allenfp@gmail.com>
2019-01-24 06:51:28 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,14 @@
 def prime?(num)
   half_num = num / 2
   num_range = 2..half_num
-
   is_prime = true
+
   num_range.each do |element|
     if num % element == 0
       is_prime = false
     end
   end
+
   puts(is_prime)
   is_prime
 end
